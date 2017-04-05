f42fd6a5e03cc9a8566c2ef2ccd80bd6881e6e08
patrickmontalto <pmontalto@gmail.com>
2017-04-05 15:19:00 -0400

    remove dependency. update readme

--- Example/Pods/Target Support Files/SwiftJSONKit/SwiftJSONKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftJSONKit : NSObject
-@end
-@implementation PodsDummy_SwiftJSONKit
-@end
--- Example/Tests/NetworkClientTests.swift
+++ Example/Tests/NetworkClientTests.swift
@@ -1,6 +1,5 @@
 import XCTest
 @testable import SwiftNetworking
-import SwiftJSONKit
 
 final class NetworkClientTests: XCTestCase {
     
